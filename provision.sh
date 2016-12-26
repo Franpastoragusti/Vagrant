479e2a6cdc2a609dabb794fd5f889af2232145e8
Franpastoragusti <franciscopa91@gmail.com>
2016-12-26 19:13:49 +0100

    3c Funcionando

--- provision.sh
+++ provision.sh
@@ -1,8 +1,12 @@
 #instalaciones requeridas
 apt-get -y update
 sudo apt-get -y install apache2
-sudo apt-get install git
+sudo apt-get -y install git
 
+#borramos posibles carpetas con el mismo nombre y la creada por apache
+sudo rm -rf /var/www/sitio1
+sudo rm -rf /var/www/sitio2
+sudo rm -rf /var/www/index.html
 #clonar las paginas de ejemplo
 git clone https://github.com/Franpastoragusti/sitio1.git /var/www/sitio1
 git clone https://github.com/Franpastoragusti/sitio2.git /var/www/sitio2
@@ -14,4 +18,5 @@ sudo mv /var/www/sitio2/sitio2.conf /etc/apache2/sites-available/sitio2.conf
 cd /etc/apache2/sites-available/
 sudo a2ensite sitio1.conf
 sudo a2ensite sitio2.conf
+sudo a2dissite default
 sudo service apache2 reload
