91d06056a9131d146131e3adf0b11712be0fee29
Franpastoragusti <franciscopa91@gmail.com>
2017-01-03 19:07:20 +0100

    principio wordpress

--- provision.sh
+++ provision.sh
@@ -1,22 +1,20 @@
 #instalaciones requeridas
-apt-get -y update
+sudo apt-get -y update
+
+# Instalación de  Apache
+
 sudo apt-get -y install apache2
-sudo apt-get -y install git
 
-#borramos posibles carpetas con el mismo nombre y la creada por apache
-sudo rm -rf /var/www/sitio1
-sudo rm -rf /var/www/sitio2
-sudo rm -rf /var/www/index.html
-#clonar las paginas de ejemplo
-git clone https://github.com/Franpastoragusti/sitio1.git /var/www/sitio1
-git clone https://github.com/Franpastoragusti/sitio2.git /var/www/sitio2
+# Instalación de MySQL, rellenando el prompt y asignando password a root
 
-#movemos los archivos de configuracion al apache
-sudo mv /var/www/sitio1/sitio1.conf /etc/apache2/sites-available/sitio1.conf
-sudo mv /var/www/sitio2/sitio2.conf /etc/apache2/sites-available/sitio2.conf
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
 
-cd /etc/apache2/sites-available/
-sudo a2ensite sitio1.conf
-sudo a2ensite sitio2.conf
-sudo a2dissite default
-sudo service apache2 reload
+# Instalación de PHP
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+# Instalación de Wordpress
+cd /var/www
+wget http://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz 
